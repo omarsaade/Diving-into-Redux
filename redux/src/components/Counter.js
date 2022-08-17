58179ec7a54b81bcc4b767248a897ded8ab46a71
omar saade <omarsaadeh11@gmail.com>
2022-08-17 14:56:35 +0300

    react

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from 'react-redux'
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
 
   const incrementHandler = () => {
@@ -13,26 +14,37 @@ const Counter = () => {
     dispatch({ type: "increment" });
   }
 
+
+
+  const increaseHandler = () => {
+    //payload 7umulah
+    dispatch({ type: "increaseby5", amount: 5 });
+  }
+
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   }
 
 
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
+  //reference type احاله و اسناد
+  // primitive جاهل بدائي  
   return (
 
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+
+        <button onClick={increaseHandler}>Increase By 5</button>
+
         <button onClick={decrementHandler}>Decrement</button>
       </div>
-
-
-
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
 
@@ -40,4 +52,5 @@ const Counter = () => {
 };
 
 export default Counter;
+
 
--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,23 +1,94 @@
 import { legacy_createStore as createStore } from 'redux'
 
 
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
-const counterReducer = (state = { counter: 0 }, action) => {
+
+const initialState = { counter: 0, showCounter: true };
+
+
+const counterReducer = (state = initialState, action) => {
 
     if (action.type === "increment") {
-        return { counter: state.counter + 1 }
+        return {
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        };
     }
+
+
+
+    if (action.type === "increaseby5") {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
+    }
+
+
     if (action.type === "decrement") {
-        return { counter: state.counter - 1 }
+        return {
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
     }
+
+
+    if (action.type === "toggle") {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
+        }
+    }
+
+
+
     return state;
 }
 
 
-const store = createStore(counterReducer);
+
+
+const store = createStore(counterReducer, enhancer());
 
 //connect my react app to that store
 //provide this store to the react app
 export default store;
 
 
+
+
+
+
+
+/* function counterReducer(state = initialState, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    case "decrement":
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    case "increase":
+      return {
+        ...state,
+        counter: state.counter + action.amount
+      }
+    case "reset":
+      return {
+        ...state,
+        counter: 0
+      }
+    case "toggleCounter":
+      return {
+        ...state,
+        showCounter: !state.showCounter
+      }
+    default: return state
+  }
+}
+*/
