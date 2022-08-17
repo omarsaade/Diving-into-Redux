02e58b5ae8c9c82cb8ee5cbbf69fb3c4f4f8bf1d
omar saade <omarsaadeh11@gmail.com>
2022-08-17 19:36:09 +0300

    react

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,3 +1,63 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { counterActions } from '../store/index';
+import classes from './Counter.module.css';
+import React from 'react'
+
+
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  }
+
+
+
+  const increaseHandler = () => {
+    //payload 7umulah
+    dispatch(counterActions.increase(10));
+  }
+
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  }
+
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  //reference type احاله و اسناد
+  // primitive جاهل بدائي  
+  return (
+
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+
+        <button onClick={increaseHandler}>Increase By 5</button>
+
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+
+  );
+};
+
+export default Counter;
+
+
+//__________________________________________________________________________________________________________
+//                                        WITHOUT REDUX TOOLKIT
+/*
 import classes from './Counter.module.css';
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
@@ -52,5 +112,4 @@ const Counter = () => {
 };
 
 export default Counter;
-
-
+*/
--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,65 +1,114 @@
-import { legacy_createStore as createStore } from 'redux'
-
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
 
 const initialState = { counter: 0, showCounter: true };
 
-
-const counterReducer = (state = initialState, action) => {
-
-    if (action.type === "increment") {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
-    }
-
-
-
-    if (action.type === "increaseby5") {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
         }
     }
-
-
-    if (action.type === "decrement") {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-
-
-    if (action.type === "toggle") {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-
-
-
-    return state;
-}
+});
 
 
 
 
-const store = createStore(counterReducer, enhancer());
+const store = configureStore({ reducer: counterSlice.reducer }, enhancer());
 
-//connect my react app to that store
-//provide this store to the react app
+export const counterActions = counterSlice.actions;
+
+
 export default store;
 
 
 
 
+//___________________________________________________________________________________
+//                        WITHOUT REACT TOOLKIT
+
+// import { legacy_createStore as createStore } from 'redux'
 
 
+// const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+
+
+// const initialState = { counter: 0, showCounter: true };
+
+
+// const counterReducer = (state = initialState, action) => {
+
+//     if (action.type === "increment") {
+//         /*you should never super important never mutate the state, the existing state.
+//         You should never change the existing state. Instead, always override it
+//         by returning a brand new state object.*/
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         };
+//     }
+
+
+
+//     if (action.type === "increaseby5") {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+
+//     if (action.type === "decrement") {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+
+//     if (action.type === "toggle") {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter
+//         }
+//     }
+
+
+
+//     return state;
+// }
+
+
+
+
+// const store = createStore(counterReducer, enhancer());
+
+// //connect my react app to that store
+// //provide this store to the react app
+// export default store;
+
+
+
+
+
+
+
+
+//____________________________________________________________________________________________
+                        //  ANOTHER WAY TO WRITING THE REDUCER FUNCTION
 
 /* function counterReducer(state = initialState, action) {
   switch (action.type) {
